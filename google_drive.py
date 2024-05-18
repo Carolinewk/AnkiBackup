776871ad9cb4cf1c3c2e454ec86a86de5b1be50a
Thiago Nebuloni <thiago.nebuloni@gmail.com>
2024-05-18 18:28:57 -0300

    added code to identify and use client_secret_file in current folder. added assert to service.files

--- google_drive.py
+++ google_drive.py
@@ -41,16 +41,22 @@ def create_service(client_secret_file, api_name, api_version, *scopes):
         return None
 
 
-# "client_secret"
-
-# nebuzoka
-#CLIENT_SECRET_FILE = "client_secret_566868905417-g3difppfkga03a99hdvofmnguokj4hgn.apps.googleusercontent.com.json"
-CLIENT_SECRET_FILE= "client_secret_860763931823-ormt3p4dh5r6gfs2fb9pgq9186ocmnms.apps.googleusercontent.com.json"
-
 API_NAME = "drive"
 API_VERSION = "v3"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
+# lista pasta atual e encontra arquivo client secret
+CLIENT_SECRET_FILE = ""
+home_dir = os.getcwd()
+files_in_home_dir = os.listdir(home_dir)
+for file in files_in_home_dir:
+    if "client_secret" in file:
+        CLIENT_SECRET_FILE = file
+
+if not CLIENT_SECRET_FILE:
+    CLIENT_SECRET_FILE = input(
+        "Credenciais não encontradas. Indique o caminho para o arquivo: "
+    )
 
 service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
@@ -67,15 +73,16 @@ def upload(
 
     media = MediaFileUpload("{0}".format(file_name), mimetype=mime_type)
 
+    assert service is not None
     service.files().create(body=file_metadata, media_body=media, fields="id").execute()
 
 
 def create_folder(folder_name):
-
     file_metadata = {
-        'name': folder_name,
-        'mimeType': 'application/vnd.google-apps.folder'
+        "name": folder_name,
+        "mimeType": "application/vnd.google-apps.folder",
     }
 
-    folder = service.files().create(body=file_metadata, fields='id').execute()
-    return folder['id']
+    assert service is not None
+    folder = service.files().create(body=file_metadata, fields="id").execute()
+    return folder["id"]
